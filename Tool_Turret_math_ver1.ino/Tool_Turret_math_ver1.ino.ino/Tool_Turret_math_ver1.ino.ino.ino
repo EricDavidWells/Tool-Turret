e3d9706dc065673591969c17da4b6c33020334b5
Eric Wells <ewells@zaber.com>
2017-05-31 17:25:05 -0700

    added trapezoid function feature

--- Tool_Turret_math_ver1.ino/Tool_Turret_math_ver1.ino.ino/Tool_Turret_math_ver1.ino.ino.ino
+++ Tool_Turret_math_ver1.ino/Tool_Turret_math_ver1.ino.ino/Tool_Turret_math_ver1.ino.ino.ino
@@ -7,8 +7,8 @@
 #define STEPPIN 3   // moves the motor one microstep per pulse
 #define DIRPIN 2    // specifies which direction the motor will turn 
 
-int step_size = 2;
-int res = 4;
+int step_size = 4;
+int res = 2;
 
 int M0_state;
 int M1_state;
@@ -17,14 +17,19 @@ int M2_state;
 float pos = 0;
 float vel = 0;
 
-float accel = 1440;   // the constant acceleration during the trapezoidal velocity profile in deg/s^2
-float vel_max = 360;    // the max velocity during the trapezoidal velocity profile in deg/s^2
+float tool_accel = 1000;   // the constant acceleration during the trapezoidal velocity profile in deg/s^2
+float tool_vel_max = 360;    // the max velocity during the trapezoidal velocity profile in deg/s^2
 
+float backstep = 4.5;    // the amount in degrees to step backwards into the pawl.  Make sure it lies on an exact multiple of the step size. e.g. if resolution is 2 make sure is a multiple of 0.9
+float backstep_accel = 50;
+float backstep_vel_max = 100;
 
 float pos_target = 0;
 float pulse_timer = 0;
 long pulse_count = 0;
-long pulse_target = 0;
+float pulse_target = 0;
+
+bool rem_flag = 0;
 
 void setup() {
   
@@ -40,8 +45,8 @@ void setup() {
   pinMode(SLEEPPIN, OUTPUT);
   pinMode(STEPPIN, OUTPUT);
   pinMode(DIRPIN, OUTPUT);
-  digitalWrite(DIRPIN, HIGH);
   
+  digitalWrite(DIRPIN, HIGH);
   digitalWrite(ENABLEPIN, LOW);
   digitalWrite(RESETPIN, HIGH);
   digitalWrite(SLEEPPIN, HIGH);
@@ -54,62 +59,30 @@ void setup() {
   digitalWrite(M1PIN, M1_state);
   digitalWrite(M2PIN, M2_state);
 
-  accel *= res;
-  vel_max *= res;
+  tool_accel *= res;
+  tool_vel_max *= res;
+  backstep_accel *= res;
+  backstep_vel_max *= res;
+  
 }
 
 void loop() {
   serial_read();
 
   if (pulse_target != pulse_count){
-          
-    float t1 = vel_max/accel;
-    float p1 = accel*pow(t1,2)/2;
-    float p2 = pulse_target - p1;
-
-    if (p1 > pulse_target/2){
-      p1 = pulse_target/2;
-      p2 = pulse_target/2;
+    if (rem_flag == 0){
+      pulse_target = ceil(pulse_target);
+      rem_flag = 1;
     }
-    
-    long timer = micros();
-    long pulse_timer = micros();
-
-    Serial.println(p1);
-    Serial.println(p2);
-
-    pulse();
-    while(pulse_count < pulse_target){
-
-      if (pulse_count < p1){
-        vel = accel  * sqrt(2*pulse_count/accel);
-      }
-      if (pulse_count > p2){
-        vel = accel * sqrt(2*(pulse_target-pulse_count)/accel);
-      }
- 
-      if (micros() - pulse_timer > 1000000/vel){
-        pulse();
-        pulse_timer = micros();
-//        Serial.print("position: ");
-//        Serial.print(pulse_count);
-//        Serial.print("  velocity: ");
-//        Serial.print(vel);
-//        Serial.print('\n');
-      } 
-
-//      delayMicroseconds(100);
+    if (rem_flag == 1){
+      pulse_target = floor(pulse_target);
+      rem_flag = 0;
     }
-
-    pos = 0;
-    pulse_count = 0;
-    pulse_target = 0;
-    pos_target = 0;
-    vel = 0;
-    
+    trapezoid(pulse_target, 1, tool_accel, tool_vel_max);
+    trapezoid(backstep*res/1.8, 0, backstep_accel, backstep_vel_max);
   }
   
-
+  
   
 //  delayMicroseconds(2);
 //  digitalWrite(STEPPIN, LOW);
@@ -120,11 +93,55 @@ void loop() {
   
 }
 
+void trapezoid(long p3, bool dir, float accel, float vel_max){
+  
+  digitalWrite(DIRPIN, dir);  
+  delayMicroseconds(10);
+          
+  float t1 = vel_max/accel;
+  float p1 = accel*pow(t1,2)/2;
+  float p2 = p3 - p1;
+
+  if (p1 > p3/2){
+    p1 = p3/2;
+    p2 = p3/2;
+  }
+  
+  long timer = micros();
+  long pulse_timer = micros();
+
+  pulse();
+  while(pulse_count < p3){
+
+    if (pulse_count < p1){
+      vel = accel  * sqrt(2.00*pulse_count/accel);
+    }
+    if (pulse_count > p2){
+      vel = accel * sqrt(2.00*(p3-pulse_count)/accel);
+    }
+
+    if (micros() - pulse_timer > 1000000/vel){
+      pulse();
+      pulse_timer = micros();
+//        Serial.print("position: ");
+//        Serial.print(pulse_count);
+//        Serial.print("  velocity: ");
+//        Serial.print(vel);
+//        Serial.print('\n');
+    }
+  }
+    pos = 0;
+    pulse_count = 0;
+    pulse_target = 0;
+    vel = 0;
+    p3 = 0;
+}
+
 void pulse(){
   digitalWrite(STEPPIN, HIGH);
-  delayMicroseconds(2);
+  delayMicroseconds(4);
   digitalWrite(STEPPIN, LOW);
-  delayMicroseconds(2);
+  delayMicroseconds(4);
   pos += 1.8/res;
   pulse_count += 1;
 }
